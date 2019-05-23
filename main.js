9069b52341a4187a819adbe049e3b5c41b24beb6
Maximilian Gonzalez <thamaxlg@gmail.com>
2019-05-22 19:27:56 -0700

    skeleton of garden extension

---
+++ main.js
@@ -0,0 +1,6 @@
+console.log('hello')
+
+let image = document.createElement('img');
+image.src = 'https://cdn.pixabay.com/photo/2018/02/08/22/27/flower-3140492_960_720.jpg';
+image.classList.add('floating-plant')
+document.body.appendChild(image);
